4f8970cfa23546761415e4873bcd8db64a5ecd38
willem.thorbecke@students.olin.edu <willem.thorbecke@students.olin.edu>
2016-11-10 17:54:50 -0500

    latest bezier

--- bezier.py
+++ bezier.py
@@ -6,6 +6,19 @@ from xml.dom import minidom
 from svgpathtools import Path, Line, QuadraticBezier, CubicBezier, Arc, parse_path
 
 
+
+def para_cubic_bezier(P0,P1,P2,P3,t):
+
+	"spits out y-val as a function of x, where 0,0 is the start of the curve"
+
+	x = (1-t)**3*P0.real + 3*t*(1-t)**2*P1.real + 3*t**2*(1-t)*P2.real + t**3*P3.real
+	y = (1-t)**3*P0.imag + 3*t*(1-t)**2*P1.imag + 3*t**2*(1-t)*P2.imag + t**3*P3.imag
+
+	
+	
+	return x,y
+
+
 def cubic_bezier(P0,P1,P2,P3,x):
 
 	"spits out y-val as a function of x, where 0,0 is the start of the curve"
@@ -18,10 +31,8 @@ def cubic_bezier(P0,P1,P2,P3,x):
 	roots = [t3,t2,t1,t0]
 	zeroes = np.roots(roots)
 
-	# print(zeroes)
-
 	zeroes = [val for val in zeroes if val >= 0 and val <= 1]
-	# print(zeroes)
+
 	t = float(zeroes[0])
 
 	y = (1-t)**3 * P0.imag+ 3*(1-t)**2 * t * P1.imag + 3*(1-t) * t**2 * P2.imag + t**3 * P3.imag
@@ -36,14 +47,40 @@ def plot_bezier(bezier):
 	P2 = (bezier.control2)
 	P3 = (bezier.end)
 
-	x_range = np.linspace(P0.real,P3.real,10000)
+	x_range = np.linspace(P0.real,P3.real,100)
 	y_range = []
 
 	for x in x_range:
-		y_range.append(cubic_bezier(P0, P1, P2, P3, x))
+		y_range.append(-cubic_bezier(P0, P1, P2, P3, x))
 
 	plt.plot(x_range,y_range)
 
+
+def plot_bezier_para(bezier):
+
+	P0 = (bezier.start)
+	P1 = (bezier.control1)
+	P2 = (bezier.control2)
+	P3 = (bezier.end)
+
+	t_range = np.linspace(0,1,1000)
+	x_range = []
+	y_range = []
+
+	print(para_cubic_bezier(P0, P1, P2, P3, .999999))
+
+	for t in t_range:
+	
+		x = para_cubic_bezier(P0, P1, P2, P3, t)[0]
+
+		y = -para_cubic_bezier(P0, P1, P2, P3, t)[1]
+
+		x_range.append(x)
+		y_range.append(y)
+
+	print(x_range)
+	print(y_range)
+	plt.plot(x_range,y_range)
 
 def parse_svg(svg_name):
 
@@ -53,7 +90,7 @@ def parse_svg(svg_name):
 	doc.unlink()
 
 	print(str(path_strings))
-	
+
 	paths =  parse_path(str(path_strings)) #"M 297.14286 478.07649 C 354.28571 14.28571 425.71428 -120 425.71428 -120"))
 
 	print(paths) 
@@ -63,11 +100,12 @@ def parse_svg(svg_name):
 		if type(path) is CubicBezier:
 			print('yooo')
 			print(path)
-			plot_bezier(path)
+			plot_bezier_para(path)
+			#plot_bezier(path)
 
 		elif type(path) is Line:
 			print('booo')
 			continue
 
-parse_svg('drawing_2.svg')
+parse_svg('drawing_5.svg')
 plt.show()
